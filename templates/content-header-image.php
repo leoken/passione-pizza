963248ddb8782ea9e70b088da0c6a455f1d211f7
Erik James Albaugh <erik@maintainweb.co>
2014-03-22 10:29:11 -0700

    First Major Push
    WooCommerce Templates Added
    Header Image Area Added
    Bootswatch LESS files Added

---
+++ templates/content-header-image.php
@@ -0,0 +1,31 @@
+
+    <div id="header-image" class="">
+      <div class="header-inner">
+<?php 
+ 
+$image = get_field('header_image');
+ 
+if( !empty($image) ): 
+ 
+  // vars
+  $url = $image['url'];
+  $title = $image['title'];
+  $alt = $image['alt'];
+  $caption = $image['caption'];
+ 
+  // thumbnail
+  $size = 'header-image';
+  $thumb = $image['sizes'][ $size ];
+  $width = $image['sizes'][ $size . '-width' ];
+  $height = $image['sizes'][ $size . '-height' ]; ?>
+ 
+    
+<style>
+ #header-image {
+  background-image: url(<?php echo $thumb; ?>);
+}
+</style>
+<?php endif; ?>
+
+      </div>
+    </div>
