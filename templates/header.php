db7cdb7ba0730bb5bf917dc8b771f8f1d9d18da5
Erik James Albaugh <erik@maintainweb.co>
2014-08-07 01:58:23 -0700

    Header Update

---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<header class="banner navbar navbar-default navbar-custom navbar-fixed-top" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <?php get_template_part('templates/content', 'branding'); ?>
+      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    </div>
+
+    <nav class="collapse navbar-collapse" role="navigation">
+      <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+        endif;
+      ?>
+      <?php
+        if (has_nav_menu('secondary_navigation')) :
+          wp_nav_menu(array('theme_location' => 'secondary_navigation', 'menu_class' => 'nav navbar-nav navbar-right'));
+        endif;
+      ?>
+    </nav>
+  </div>
+</header>
